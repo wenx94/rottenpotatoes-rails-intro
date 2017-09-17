a78a4ddf3b38c8ba09a0ac03e0f5e84e9dcaea7d
Xintong Wen <wenb@berkeley.edu>
2017-09-17 23:24:31 +0000

    added ratings method in movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
     
     if params[:sort]
       @sort_by = params[:sort]
